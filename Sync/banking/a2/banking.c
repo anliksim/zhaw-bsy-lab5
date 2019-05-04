fe79ae9fc8ab92e0d1f47dc5f821ebf305c47663
Simon Anliker <anliksim@users.noreply.github.com>
2019-05-04 15:57:56 +0200

    3.2.2 Synch branches

---
+++ Sync/banking/a2/banking.c
@@ -0,0 +1,131 @@
+//******************************************************************************
+// Course:  BSy
+// File:    banking.c
+// Author:  M. Thaler, ZHAW
+// Purpose: locking mechanisms
+// Version: v.fs19
+//******************************************************************************
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <pthread.h>
+
+#include "banking.h"
+
+//******************************************************************************
+
+typedef struct account_struct_ {
+    long int balance;
+    pthread_mutex_t acntLock;
+} Account;
+
+typedef struct branch_struct {
+    Account *accounts;
+    pthread_mutex_t branchLock; 
+} Branch;
+
+//******************************************************************************
+
+static Branch *Bank;
+static int nBranches, nAccounts;
+
+//******************************************************************************
+// banking functions
+
+void makeBank(int num_branches, int num_accounts) {
+    nBranches = num_branches;
+    nAccounts = num_accounts;
+    Bank = (Branch *)malloc(nBranches * sizeof(Branch));
+
+    pthread_mutexattr_t attr;
+    pthread_mutexattr_init(&attr);
+
+    for (int  i = 0; i < nBranches; i++) {
+        Bank[i].accounts = (Account *)malloc(nAccounts * sizeof(Account));
+        pthread_mutex_init(&(Bank[i].branchLock), &attr);
+        for (int j = 0; j < nAccounts; j++) {
+            Bank[i].accounts[j].balance = 0;
+            pthread_mutex_init((&(Bank[i].accounts[j].acntLock)), &attr);
+        }
+    }
+}
+
+void deleteBank(void) {
+    for (int i = 0; i < nBranches; i++)
+        free(Bank[i].accounts);
+    free(Bank);
+    nBranches = nAccounts = 0;
+}
+
+long int withdraw(int branchNr, int accountNr, long int value) {
+    int rv, tmp;
+    rv = 0;
+
+    // $ lock branch
+    Branch* branch = &Bank[branchNr];
+    pthread_mutex_lock(&branch->branchLock);
+    // $ lock account for atomic withdraw
+    Account* acc = &branch->accounts[accountNr];
+    pthread_mutex_lock(&acc->acntLock);
+
+    tmp = acc->balance - value;
+    if (tmp >= 0) {
+        acc->balance = tmp;
+        rv = value;
+    }
+    pthread_mutex_unlock(&branch->branchLock);
+    pthread_mutex_unlock(&acc->acntLock);
+
+    return rv;   
+}
+
+void deposit(int branchNr, int accountNr, long int value) {
+    // $ lock branch
+    Branch* branch = &Bank[branchNr];
+    pthread_mutex_lock(&branch->branchLock);
+
+    // $ lock account for deposit
+    Account* acc = &branch->accounts[accountNr];
+    pthread_mutex_lock(&acc->acntLock);
+
+    acc->balance += value;
+
+    pthread_mutex_unlock(&acc->acntLock);
+    pthread_mutex_unlock(&branch->branchLock);
+}
+
+void transfer(int fromB, int toB, int accountNr, long int value) {
+    int money = withdraw(fromB, accountNr, value);
+    if (money >= 0)
+        deposit(toB, accountNr, money);
+}
+
+void checkAssets(void) {
+    static long assets = 0;
+    long sum = 0;
+    for (int i = 0; i < nBranches; i++) {
+        for (int j = 0; j < nAccounts; j++) {
+            // $ lock account to get correct value
+            Account* acc = &Bank[i].accounts[j];
+            pthread_mutex_lock(&acc->acntLock);
+
+            sum += (long)acc->balance;
+
+            pthread_mutex_unlock(&acc->acntLock);
+        }
+    }
+    if (assets == 0) {
+        assets = sum;
+        printf("Balance of accounts is: %ld\n", sum);
+    }
+    else {
+        if (sum != assets)
+            printf("Balance of accounts is: %ld ... not correct\n", sum);
+        else
+            printf("Balance of accounts is: %ld ... correct\n", assets);
+    }
+}
+
+//******************************************************************************
+
